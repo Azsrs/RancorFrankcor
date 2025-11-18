bd1720c87645f63e69ddb6bd212791ebcc254319
Azsrs <farzanehfrank@icloud.com>
2025-11-18 10:39:58 +0100

    Start FileSystem

--- RancorFrankcor/Program.cs
+++ RancorFrankcor/Program.cs
@@ -1,5 +1,48 @@
 ﻿using RancorFrankcor;
+using System.IO;
+//Använder Typewriter klassen för att skapa dialog
 
-TypeWriter t1 = new TypeWriter();
-t1.Text = "New Super Frank Fighter Turbo Ultra Z EX 64 Shippuden";
-t1.Talk();
+TypeWriter Title = new TypeWriter(); //Intro
+Title.Text = "New Super Frank Fighter Turbo Ultra Z EX 64 Shippuden\nPress ENTER to start";
+Title.TextSpeed = 40;
+Title.Talk();
+Console.ReadLine();
+
+TypeWriter Rules = new TypeWriter(); //Regler
+Rules.Text = "\nYou will create a powerful fighter and face off against a mighty opponent!\nIf your HP reaches 0 you lose";
+Rules.TextSpeed = 40;
+Rules.Talk();
+Console.ReadLine();
+
+Console.Clear();
+
+TypeWriter Loadq = new TypeWriter(); //Frågar om att ladda save?
+Loadq.Text = "Would you like to load a save?";
+Loadq.TextSpeed = 20;
+Loadq.Talk();
+string LoadSave = Console.ReadLine().ToLower();
+
+while (LoadSave != "yes" && LoadSave != "no") //Om ej accepterat svar
+{
+
+    Console.WriteLine("Load a save? Please answer Yes or No?");
+    LoadSave = Console.ReadLine();
+}
+
+if (LoadSave == "yes")
+{
+    if (Directory.Exists(@"Savegames"))
+    {
+        Console.WriteLine("Please pick save to load");
+        foreach (string filename in Directory.GetFiles(@"Savegames"))
+        {
+            Console.WriteLine(filename);
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("No save file found, starting game");
+        Thread.Sleep(150);
+    }
+}
--- RancorFrankcor/TypeWriter.cs
+++ RancorFrankcor/TypeWriter.cs
@@ -2,19 +2,20 @@ using System;
 
 namespace RancorFrankcor;
 
-public class TypeWriter
+public class TypeWriter //Klass för att lätt skapa text med typewriter effekt
 {
-    public string Text;
-    public int i;
+    public string Text; //Vad som ska sägas
+    public int i; 
 
+    public int TextSpeed; //Hur lång tid mellan att varje bokstav skrivs ut
     public void Talk()
     {
 
 
-        for (int i = 0; i < Text.Length; i++)
+        for (int i = 0; i < Text.Length; i++) //Skriver ut bokstäver en efter en till hela texten har skrivits ut
         {
-            Console.Write(Text[i]);
-            Thread.Sleep(40);
+            Console.Write(Text[i]); //Varje räknevärde motsvarar ett tecken i texten, skrivs ut varje loop
+            Thread.Sleep(TextSpeed);
         }
     }
 }
